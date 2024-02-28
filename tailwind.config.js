ad1cb6019e51bc01f8eacc82cf7ca8c9d02df022
ArtemDanilov <artemdanilow95@gmail.com>
2024-02-28 10:41:31 +0100

    Prepare project for the dev: - Install fonts - Install scss and alpine.js - Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ export default {
     ],
 
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'primary': ['Raleway', 'sans-serif'],
+                'secondary': ['Harmony', 'serif'],
+            },
+            colors: {
+                'white': '#F8F7F4',
+                'black': '#091326',
+                'brown': '#5D5349',
+                'gray': '#C2C7D1',
+
+                'beige-200': '#E8DBCD',
+                'beige-100': '#EEE7E1',
+            }
+        },
     },
 
     plugins: [
