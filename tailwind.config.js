2edeb1cdf65b958b0c5ed478c8d46371b853699c
ArtemDanilov <artemdanilow95@gmail.com>
2024-02-29 10:35:41 +0100

    Update layout and create header

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,58 @@ export default {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                'primary': ['Raleway', 'sans-serif'],
-                'secondary': ['Harmony', 'serif'],
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                '2xl': '5.5rem',
             },
-            colors: {
-                'white': '#F8F7F4',
-                'black': '#091326',
-                'brown': '#5D5349',
-                'gray': '#C2C7D1',
+        },
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1440px',
+        },
 
-                'beige-200': '#E8DBCD',
-                'beige-100': '#EEE7E1',
+    extend: {
+        fontFamily: {
+            'primary': ['Raleway', 'sans-serif'],
+            'secondary': ['Harmony', 'serif'],
+        },
+        colors: {
+            'black': '#091326',
+            'brown': '#5D5349',
+            'gray': '#C2C7D1',
+            
+            'beige-50': '#F8F7F4',
+            'beige-100': '#EEE7E1',
+            'beige-200': '#E8DBCD',
+        },
+        margin: {
+            '7.5': '1.875rem',  // 30px
+        },
+        keyframes: {
+            fadeIn: {
+                "0%": { opacity: "0" },
+                "100%": { opacity: "1" },
+            },
+            fadeOut: {
+                "0%": { opacity: "1" },
+                "100%": { opacity: "0" },
             }
         },
+        animation: {
+            fadeIn: "fadeIn 500ms forwards",
+            fadeOut: "fadeOut 500ms forwards",
+        }
     },
+},
 
     plugins: [
         require('@tailwindcss/typography'),
     ],
-};
+}
